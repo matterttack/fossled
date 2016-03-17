4b74fbd66bf445d4674a1e7cbe05f76af7c12aea
Matthew Baker <mattsalias@gmail.com>
2016-03-17 18:08:45 +0100

    add prerender.io and update public dist

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(require('prerender-node'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,8 +33,8 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
+// error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
