1c83e8f4ffa88039069dbeaf46cc4c798565eb0b
Matthew Baker <mattsalias@gmail.com>
2016-08-25 01:18:28 +0100

    Remove product page bug regarding variabels

--- routes/index.js
+++ routes/index.js
@@ -272,9 +272,9 @@ var request = sg.emptyRequest({
 
 
 sg.API(request, function(error, response) {
-  // console.log(response.statusCode);
-  // console.log(response.body);
-  // console.log(response.headers);
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
     if (error) {
       console.log(error)
       return next(error);
